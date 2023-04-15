af961863dccd10aadfe26fecb764e1f018ebaf8d
fernando-izar <fizar@me.com>
2023-04-15 15:42:44 -0300

    feat: routes building, login, map

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD09e965HWRM5TfdoaIm1qXEuMTnPZSzWw",
+  authDomain: "bim-app-fc.firebaseapp.com",
+  projectId: "bim-app-fc",
+  storageBucket: "bim-app-fc.appspot.com",
+  messagingSenderId: "49023160366",
+  appId: "1:49023160366:web:52290d6c918dbd8df10fbf",
+};
+
+initializeApp(firebaseConfig);
+
 root.render(
   <React.StrictMode>
     <App />
